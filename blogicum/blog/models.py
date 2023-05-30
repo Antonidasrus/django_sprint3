0bd52efa95cace63c47f257392e48e62450b6c73
Коновалов Антон Романович <akonovalov@stan-group.com>
2023-05-30 20:22:45 +0300

    PEP8 correct?

--- blogicum/blog/models.py
+++ blogicum/blog/models.py
@@ -18,8 +18,7 @@ class PublishedModel(models.Model):
         help_text=is_published_help_text)
     created_at = models.DateTimeField(
         auto_now_add=True,
-        verbose_name='Добавлено'
-        )
+        verbose_name='Добавлено')
 
     class Meta:
         abstract = True
@@ -28,16 +27,13 @@ class PublishedModel(models.Model):
 class Category(PublishedModel):
     title = models.CharField(
         max_length=256,
-        verbose_name='Заголовок'
-        )
+        verbose_name='Заголовок')
     description = models.TextField(
-        verbose_name='Описание'
-        )
+        verbose_name='Описание')
     slug = models.SlugField(
         unique=True,
         verbose_name='Идентификатор',
-        help_text=slug_help_text
-        )
+        help_text=slug_help_text)
 
     class Meta:
         verbose_name = 'категория'
@@ -71,26 +67,22 @@ class Post(PublishedModel):
         )
     pub_date = models.DateTimeField(
         verbose_name='Дата и время публикации',
-        help_text=pub_date_help_text
-        )
+        help_text=pub_date_help_text)
 
     author = models.ForeignKey(
         User,
         on_delete=models.CASCADE,
-        verbose_name='Автор публикации'
-        )
+        verbose_name='Автор публикации')
     location = models.ForeignKey(
         Location,
         on_delete=models.SET_NULL,
         null=True,
-        verbose_name='Местоположение'
-        )
+        verbose_name='Местоположение')
     category = models.ForeignKey(
         Category,
         on_delete=models.CASCADE,
         null=True,
-        verbose_name='Категория'
-        )
+        verbose_name='Категория')
 
     class Meta:
         verbose_name = 'публикация'
